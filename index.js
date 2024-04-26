d7184609c5a48bbeea1c156155358b2ed070ba85
hanjongseop <whd4355@gmail.com>
2024-04-26 14:00:54 +0900

    api html등록 및 post클릭시 id출력

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njg3ZTZkMDRlNzg1YjBiOWRmNWZiOTZlNjAyNzFhZSIsInN1YiI6IjY2MjY3ODIxMmRkYTg5MDE4N2UzY2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m0VR-0DBlwf_cbftDltXdOACEDmLpdQ_8R3E6kmLDTg'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njg3ZTZkMDRlNzg1YjBiOWRmNWZiOTZlNjAyNzFhZSIsInN1YiI6IjY2MjY3ODIxMmRkYTg5MDE4N2UzY2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m0VR-0DBlwf_cbftDltXdOACEDmLpdQ_8R3E6kmLDTg",
+  },
 };
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(response => console.log(response.results))
-  .catch(err => console.error(err));
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.results)
 
-const movie = document.querySelector()
-const array1 = [response.results]
+    response.results.forEach(element => {
 
-array1.forEach(Element=>"#api")
-console.log(array1);
-const array = document.
+      let moviecard =document.createElement("li")
 
-const movie = document.querySelector('#api')
-movie.innerHTML = (response.results)
-console.log(movie);
+      let post =document.createElement("img")
+      post.src ="https://image.tmdb.org/t/p/w185" + element.poster_path;
+      moviecard.appendChild(post);
+      post.onclick = function(){
+        alert("id:"+element.id)
+      }
 
-const movie = document.querySelector()
-const array1 = [response.results]
+      let titleli =document.createElement("div")
+      titleli.innerText=element.title;
+      moviecard.appendChild(titleli);
+     
+      let overviewli =document.createElement("p")
+      overviewli.innerText=element.overview;
+      moviecard.appendChild(overviewli);
+      
+      let score =document.createElement("p")
+      score.textContent=element.vote_average;
+      moviecard.appendChild(score);
 
 
+      document.getElementById("api").appendChild(moviecard);
+
+
+
+
+
+    // console.log(element.title);
+    // console.log(element.overview);
+    // console.log(element.poster_path); 
+    // console.log(element.vote_average); 
+
+   
+    });
+
+  })
+  .catch((err) => console.error(err));
+
+  
