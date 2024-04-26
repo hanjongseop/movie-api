65d2bb811a1eecaf527b683c991e9c18f2d35c4d
hanjongseop <whd4355@gmail.com>
2024-04-26 19:31:10 +0900

    검색기능 반 정도 진행

--- index.js
+++ index.js
@@ -13,47 +13,85 @@ fetch(
 )
   .then((response) => response.json())
   .then((response) => {
+
+
+
     console.log(response.results)
 
     response.results.forEach(element => {
 
       let moviecard =document.createElement("li")
+      moviecard.className="movie-card"
 
       let post =document.createElement("img")
       post.src ="https://image.tmdb.org/t/p/w185" + element.poster_path;
       moviecard.appendChild(post);
       post.onclick = function(){
         alert("id:"+element.id)
+        post.className="movie-post"
       }
 
       let titleli =document.createElement("div")
       titleli.innerText=element.title;
       moviecard.appendChild(titleli);
-     
+      titleli.className="movie-titleli"
+    
       let overviewli =document.createElement("p")
       overviewli.innerText=element.overview;
       moviecard.appendChild(overviewli);
+      overviewli.className="movie-overviewli"
       
       let score =document.createElement("p")
       score.textContent=element.vote_average;
       moviecard.appendChild(score);
-
+      score.className="movie-score"
 
       document.getElementById("api").appendChild(moviecard);
 
 
-
-
-
-    // console.log(element.title);
     // console.log(element.overview);
     // console.log(element.poster_path); 
     // console.log(element.vote_average); 
-
-   
     });
 
+
+
+  
+  
+  let search = document.getElementById("search-input");
+  search.addEventListener("input", function () {
+  let searchQuery = this.value.toLowerCase();
+  let movieCards = document.querySelectorAll(".movie-card");
+  movieCards.forEach(function(card) {
+    let title = card.querySelector("div").innerText.toLowerCase();
+
+    if (title.includes(searchQuery)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
+
+
+
+    let a = document.querySelectorAll('.movie-card')
+    console.log(a);
   })
   .catch((err) => console.error(err));
 
+
+
+
+
+ 
+
+  //input에 text를 입력하면 관련 포함되는 title만 남고 사라지는 형식
+  //input에 text가 들어가면 포함(순서상관없이?순서상관있이?)하면 목록에 보이고
+  //포함 안되면 사라지는 형식?
+  //밑에 목록이 나오게?
+  //검색버튼 클릭시 실행
+ 
+
   
+
