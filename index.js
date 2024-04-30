067d326a93c4ac5f50d6fab3aa4e5cbd0ba4b1d9
hanjongseop <whd4355@gmail.com>
2024-04-30 16:22:55 +0900

    재제출

--- index.js
+++ index.js
@@ -3,22 +3,15 @@ const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njg3ZTZkMDRlNzg1YjBiOWRmNWZiOTZlNjAyNzFhZSIsInN1YiI6IjY2MjY3ODIxMmRkYTg5MDE4N2UzY2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m0VR-0DBlwf_cbftDltXdOACEDmLpdQ_8R3E6kmLDTg",
-  },
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njg3ZTZkMDRlNzg1YjBiOWRmNWZiOTZlNjAyNzFhZSIsInN1YiI6IjY2MjY3ODIxMmRkYTg5MDE4N2UzY2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m0VR-0DBlwf_cbftDltXdOACEDmLpdQ_8R3E6kmLDTg"
+  }
 };
 
-fetch(
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  options
-)
+fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
   .then((response) => response.json())
   .then((response) => {
     console.log(response.results);
 
-    //response.results를 메서드 배열 작업
-    //moviecard라는 변수와 =  li요소 추가
-    //moviecard class 이름movie-card로 생성
-    //id "api" 찾아서 moviecard요소 추가
     response.results.forEach((element) => {
       let moviecard = document.createElement("li");
       moviecard.className = "movie-card";
@@ -26,61 +19,40 @@ fetch(
 
       let post = document.createElement("div");
       moviecard.appendChild(post);
-      
-      //post라는 변수와 =  lmg요소 추가
-      //post.src = api이미지 받아오는주소 + api poster_path 값
-      //부모객체의(moviecard)에 post를추가
-      //post클릭시  alert ""발생
-      //post class 이름은 "movie-post"로 추가
+
       let postimg = document.createElement("img");
       postimg.src = "https://image.tmdb.org/t/p/w185" + element.poster_path;
       post.appendChild(postimg);
       postimg.onclick = function () {
         alert("id:" + element.id);
-        postimg.className = "movie-popostimgst";
+        postimg.className = "movie-postimg";
       };
 
-      //titleli라는 변수와 =  div 요소 추가
-      //titleli 변수에 텍스트추가 = api element.title 값
-      //부모객체(moviecard)에 titleli를추가
-      //moviecard class 이름 "movie-titleli"로 추가
       let titleli = document.createElement("div");
       titleli.innerText = element.title;
       moviecard.appendChild(titleli);
       titleli.className = "movie-titleli";
 
-      //overviewli라는 변수와 =  p 요소 추가
-      //overviewli 변수에 텍스트추가 = api element.overview 값
-      //부모객체(moviecard)에 overviewli를추가
-      //overviewli class 이름 "movie-overviewli"로 추가
       let overviewli = document.createElement("p");
       overviewli.innerText = element.overview;
       moviecard.appendChild(overviewli);
       overviewli.className = "movie-overviewli";
 
-      //score라는 변수와 =  p 요소 추가
-      //scoreaverage변수에 element.vote_average 값이 나오고 .tofixed();로 소수점 반올림
-      //score 변수에 텍스트추가 = scoreaverage 값
-      //부모객체(moviecard)에 score를 추가
-      //score class 이름 "movie-score"로 추가
       let score = document.createElement("p");
-      let scoreaverage = parseFloat(element.vote_average).toFixed(1); // 소수점 반올림
+      let scoreaverage = parseFloat(element.vote_average).toFixed(1);
       score.innerText = "평점: " + scoreaverage;
       moviecard.appendChild(score);
       score.className = "movie-score";
     });
 
     const searchInput = document.getElementById("search-input");
-
+    searchInput.focus();
 
     function performSearch() {
       let searchQuery = searchInput.value.toLowerCase();
       let movieCards = document.querySelectorAll(".movie-card");
-
       movieCards.forEach(function (card) {
-        let title = card
-          .querySelector(".movie-titleli")
-          .innerText.toLowerCase();
+        let title = card.querySelector(".movie-titleli").innerText.toLowerCase();
 
         if (title.includes(searchQuery)) {
           card.style.display = "block";
@@ -90,12 +62,7 @@ fetch(
       });
     }
 
-
-    document
-      .getElementById("search-btn")
-      .addEventListener("click", performSearch);
-
-
+    document.getElementById("search-btn").addEventListener("click", performSearch);
     searchInput.addEventListener("keypress", function (event) {
       if (event.key === "Enter") {
         performSearch();
